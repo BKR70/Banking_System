42c9c8353d6e2472ad2006909f5dac9e22215ee8
BKR70 <bilas.duet.204027@gmail.com>
2025-03-06 19:49:12 +0600

    dependency injection and mvc basic code added

---
+++ Dependency_Injection/Dependency_Injection/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependency_Injection
+{
+    public interface VehicleInterface 
+    {   
+        void Start();
+    }
+    public class Driver
+    {
+        private readonly VehicleInterface _vehicleInterface;
+        public Driver(VehicleInterface obj)
+        {
+            _vehicleInterface = obj;
+        }
+        public void ShowOutput()
+        {
+            _vehicleInterface.Start();
+        }
+
+    }
+    public class Bike : VehicleInterface
+    {
+        public void Start()
+        {
+            Console.WriteLine("Bike Started");
+        }
+    }
+    public class Bus : VehicleInterface
+    {
+        public void Start()
+        {
+            Console.WriteLine("Bus Started");
+        }
+    }
+    public class Car : VehicleInterface
+    {
+        public void Start()
+        {
+            Console.WriteLine("Car Started");
+        }
+    }
+    
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Bike obb = new Bike();
+            Car obc = new Car();
+            Bus obd = new Bus();
+            Driver ob1 = new Driver(obb);
+            ob1.ShowOutput();
+        }
+    }
+}
+
